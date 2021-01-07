9106cb74c6b245ae0180b3f1118610821ac48c29
ArfOz <arfoz1245@gmail.com>
2021-01-07 22:32:06 +0300

    like page added

---
+++ src/blog/views.py
@@ -0,0 +1,89 @@
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Comment, Post, Like
+from .forms import CommentForm, PostForm
+
+
+def post_list(request):
+    qs = Post.objects.filter(status='p')
+    context = {
+        "object_list": qs
+    }
+    return render(request, "blog/post_list.html", context)
+
+
+def post_create(request):
+    # form = PostForm(request.POST or None, request.FILES or None)
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            # form.save()
+            return redirect("blog:list")
+    context = {
+        'form': form
+    }
+    return render(request, "blog/post_create.html", context)
+
+
+def post_detail(request, slug):
+    # Post.objects.get(slug=learn-drf-3c78be2186)
+    form=CommentForm()
+    obj = get_object_or_404(Post, slug=slug)  # slug = learn-drf-3c78be2186
+    if request.method =="POST":
+        form =CommentForm(request.POST)
+        if form.is_valid:
+            comment =form.save(commit=False)
+            comment.user = request.user
+            comment.post = obj
+            comment.save()
+            return redirect("blog:detail", slug=slug)
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_detail.html", context)
+
+
+def post_update(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.user != obj.author.id:
+        # return HttpResponse("YOU ARE NOT AUTHORIZED")
+        return redirect("blog:list")
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("blog:list")
+
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
+
+
+def post_delete(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.user != obj.author.id:
+        # return HttpResponse("YOU ARE NOT AUTHORIZED")
+        return redirect("blog:list")
+    if request.method == "POST":
+        obj.delete()
+        return redirect("blog:list")
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_delete.html", context)
+
+def like(request, slug):
+    if request.method =="POST":
+        obj = get_object_or_404(Post, slug=slug)
+        like_qs =Like.objects.filter(user=request.user, post=obj)
+        if like_qs.exists():
+            like_qs[0].delete()
+        else:
+            Like.objects.create(user=request.user, post=obj)
+        return redirect("blog:detail", slug=slug)
